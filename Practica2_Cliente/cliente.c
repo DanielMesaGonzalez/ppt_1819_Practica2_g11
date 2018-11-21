5474a1c45d3505adabcbb0c2ad6b8bc88c463020
Daniel <Daniel@DESKTOP-Q0B6KD2>
2018-11-21 13:03:40 +0100

    implementacion del punto, modificacion correo y rset

--- Practica2_Cliente/cliente.c
+++ Practica2_Cliente/cliente.c
@@ -10,7 +10,10 @@ Fecha: 09/2018
 Descripción:
 	Cliente sencillo TCP para IPv4 e IPv6
 
-Autor: Juan Carlos Cuevas Martínez
+Autor: 
+
+Daniel Mesa González
+Javier Sánchez Samper
 
 *******************************************************/
 #include <stdio.h>
@@ -30,7 +33,20 @@ int main(int *argc, char *argv[])
 	char buffer_in[1024], buffer_out[1024],input[1024];
 	int recibidos=0,enviados=0;
 	int estado=S_HELO;
-	char option;
+
+	char remitente[10] = " ";
+	char destinatario[10] = " ";
+	char data[10] = " ";
+	char option="/0";
+	char fecha[100]=" ";
+	char mensaje[1024] = " ";
+	char cadena[1024] = ".";
+	char asunto[1024] = " ";
+	int comprobacion = 0;
+	int comprobacion2 = 0;
+	int comprobacion3 = 0;
+
+
 	int ipversion=AF_INET;//IPv4 por defecto
 	char ipdest[256];
 	char default_ip4[16]="127.0.0.1";
@@ -106,7 +122,7 @@ int main(int *argc, char *argv[])
 
 			if(connect(sockfd, server_in, address_size)==0){
 				printf("CLIENTE> CONEXION ESTABLECIDA CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
-			
+			//implementar recibidos
 				//Inicio de la máquina de estados
 				do{
 					switch(estado){
@@ -117,48 +133,50 @@ int main(int *argc, char *argv[])
 						//-------------------------------------
 						break;
 
-					case S_USER:
-						// establece la conexion de aplicacion 
-						printf("CLIENTE> Introduzca el usuario (enter para salir): ");
-						gets_s(input,sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
+					case S_MAIL_FROM:
+						printf("Introdude remitente: ");
+						gets_s(remitente, sizeof(remitente));
+						//faltaría immplentar rset
+						break;
 
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",SC,input,CRLF);
+					case S_RCPT:
+						printf("Introducir destinatario:");
+						gets_s(destinatario, sizeof(destinatario));
+						//faltaría immplentar rset
+
 						break;
-					case S_PASS:
-						printf("CLIENTE> Introduzca la clave (enter para salir): ");
-						gets_s(input, sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",PW,input,CRLF);
-						break;
+
 					case S_DATA:
-						printf("CLIENTE> Introduzca datos (enter o QUIT para salir): ");
-						gets_s(input, sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",ECHO,input,CRLF);
+							sprintf_s (buffer_out, sizeof(buffer_out), "DATA%s",CRLF);
 						break;
-				
+
+					case S_MENSAJE:
+						printf("Introduce el asunto: ");
+						gets_s(asunto, sizeof(asunto));
+						sprintf_s(mensaje, sizeof(mensaje), "DATE: %s%sSUBJECT: %s%sFROM: %s%sTO: %s%s%s",fecha, CRLF, asunto, CRLF, destinatario, CRLF, remitente, CRLF, CRLF );
+						sprintf_s(buffer_out, sizeof(buffer_out),"%s",mensaje);
+
+						enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
+						printf("Introducir mensaje para enviar (para finalizar introducir un punto(.)): ");
+
+						do {
+							gets_s(cadena, sizeof(cadena));
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", cadena, CRLF);
+							enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
+						} while (strcmp(cadena, ".") != 0); 
+
+						sprintf_s(buffer_out,sizeof(buffer_out), "%s%s", cadena, CRLF);
+						break;
+						
 					}
 
-					if(estado!=S_HELO){
+
 						enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
 						if(enviados==SOCKET_ERROR){
 							 estado=S_QUIT;
 							 continue;
 						}
-					}
+					
 						
 					recibidos=recv(sockfd,buffer_in,512,0);
 					if(recibidos<=0){
@@ -174,8 +192,82 @@ int main(int *argc, char *argv[])
 					}else{
 						buffer_in[recibidos]=0x00;
 						printf(buffer_in);
-						if(estado!=S_DATA && strncmp(buffer_in,OK,2)==0) 
-							estado++;  
+						if (strcmp(buffer_in, "5", 1) == 0) {
+							printf("Cerrando el servidor");
+							estado = S_QUIT;
+						}
+						else {
+							if (estado == S_HELO && strcmp(buffer_in, "2", 1) == 0) {
+								estado++;
+							}
+							else if (estado == S_MAIL_FROM && strcmp(buffer_in, "2", 1) == 0) {
+								if (strncmp(buffer_out, "rset", 4) == 0) {
+									estado = S_HELO;
+								}
+								else {
+									estado++;
+								}
+							}
+							else if (estado==S_RCPT){
+								if (strncmp(buffer_in, "2", 1) == 0) {
+									if (strncmp(buffer_out, "rset", 4) == 0) {
+										estado = S_HELO;
+									}
+									else {
+										printf("¿Desea cambiar el remitente?");
+										do {
+											option = _getche();
+											printf("%s", CRLF);
+											if (option == 'S' || option == 's') {
+												estado = S_RCPT;
+												comprobacion3 = 1;
+											}
+											else if (option == 'N' || option == 'n') {
+												estado++;
+												comprobacion3 = 1;
+											}
+											else {
+												printf("Incorrecto,vuelve a introducir una opcion (S/N)%s",CRLF);
+												comprobacion3 = 0;
+											}
+										} while (comprobacion3 == 0);
+									}
+								}
+								else {
+									printf("ERROR, no existe un destinatario en el servidor%s",CRLF);
+									estado = S_RCPT;
+								}
+							}
+							else if (estado == S_DATA && strncmp(buffer_in, "3", 1) == 0) {
+								estado++;
+							}
+							else if (estado == S_MENSAJE) {
+								if (strcmp(cadena, ".") == 0 && strncmp(buffer_in, "2", 1) == 0) {
+									printf("¿Desea enviar otro correo (S/N)?: ");
+
+									do {
+										option = _getche();
+										printf("%s",CRLF);
+										if (option == 'S' || option == 's') {
+											estado = S_MAIL_FROM;
+											comprobacion = 1;
+										}
+										else if (option == 'N' || option == 'n') {
+											estado++;
+											comprobacion = 1;
+										}
+										else {
+											printf("opcion no permitida volver a introducir%s",CRLF);
+											comprobacion = 0;
+										}
+									} while (comprobacion == 0);
+								}
+								else{
+									estado = S_MENSAJE;
+								}
+							}
+							else{}
+						}
 					}
 
 				}while(estado!=S_QUIT);		
